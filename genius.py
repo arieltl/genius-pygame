24b855ff0ed3dc0fbb7a2e4eadb9ce8b6327ccd2
ariel leventhal <arielleventhal@gmail.com>
2020-06-01 20:56:32 -0300

    adicionado comentarios

--- genius.py
+++ genius.py
@@ -3,14 +3,25 @@ from main_game import GeniusGame
 from CONFIG import WIDTH, HEIGHT, DRAW_RADIUS, INIT, END, QUIT, GAME
 from game_menu import main_menu
 from game_over_menu import game_over
+
+#class que gerencia telas do jogo
 class GameManager:
     def __init__(self):
+
+        #incializa pygame
         pygame.mixer.pre_init(44100, -16, 2)
         pygame.mixer.init()
         pygame.init()
+
+        # modificadores de dificuldade do jogo.
         self.difficulty = [False, False]
+
         window = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('LUCIANIUS!')
+        #criando um obejto de jogo
         game = GeniusGame(window, self)
+
+        #controle de fluxo das telas do jogo
         state = INIT
         while state != QUIT:
             if state == INIT:
--- main_game.py
+++ main_game.py
@@ -3,7 +3,7 @@ import pygame.gfxdraw
 from random import randint
 from math import pi,sin,cos
 from support_classes import Circle, Note
-from CONFIG import HEIGHT,WIDTH, DRAW_RADIUS, COLORS, FREQUENCIES, QUIT, END, INIT
+from CONFIG import HEIGHT,WIDTH, DRAW_RADIUS, COLORS, FREQUENCIES, QUIT, END, INIT,FPS
 import os
 
 
@@ -15,9 +15,8 @@ class GeniusGame:
         self.luci_sound = pygame.mixer.Sound(os.path.join("sprites","scream.ogg"))
         self.buzzer_sound = pygame.mixer.Sound(os.path.join("sprites","buzzer.wav"))
 
-
     def start_game(self):
-        print("startGame")
+        #reseta valor de propriedades para comecar um novo jogo
         self.sequence = []
         self.input_sequence = []
         self.circles = []
@@ -28,28 +27,29 @@ class GeniusGame:
         self.waiting_time = 300
         self.flash_time = 1000
         self.game_over = False
+        #inicia um jogo
         return self.game_loop()
 
-
+    #adciona um botao na sequencia
     def increment_sequence(self):
         round = len(self.sequence)
         if self.difficulty[1] and round % 4 == 0 and round !=0 and len(self.circles) < 8:
-            print("one more")
             next_i = len(self.circles)
             self.circles.append(Circle(50,COLORS[next_i],FREQUENCIES[next_i]))
             print(round)
             print(COLORS[round])
             self.calculate_scene()
             self.draw_scene()
-        if self.difficulty[0] and round % 4 == 0 and round !=0:
-            self.flash_time = int(self.flash_time * (0.85 if self.flash_time > 400 else 1))
+
+        if self.difficulty[0] and round % 2 == 0 and round !=0:
+            self.flash_time = int(self.flash_time * (0.85 if self.flash_time > 300 else 1))
             self.waiting_time = int(self.waiting_time * (0.95 if self.waiting_time > 200 else 1))
         
         self.sequence.append(randint(0, len(self.circles)-1))
         self.play_sequence()
         
 
-   
+   #reproduz a sequencia correta
     def play_sequence(self):
         actions = []
         for i in self.sequence:
@@ -61,19 +61,24 @@ class GeniusGame:
         await_input = {"function": self.enable_input, "arguments": (True,), "delay": 0}
         self.actions_list += actions + [await_input]
 
+    #lida com input nos botes
     def new_input(self, circle):
         self.enable_input(False)
+        #caso o tenha pressionado o botao correto
         if self.circles.index(circle) == self.sequence[self.current_index]:
+            #pisquarr o botao
             circle.flash(self.window, 300)
             await_input = {"function": self.enable_input, "arguments": (True,), "delay": 0}
             action = {"function": self.draw_scene, "delay":300}
             self.actions_list += [action,await_input]
+            #esperar proximo input ou inicar proxima rodada
             self.current_index += 1
             if self.current_index == len(self.sequence):
                 self.current_index = 0
                 action = {"function": self.increment_sequence, "delay":500}
                 self.actions_list.append(action)
         else:
+            #caso tenha pressionado botao incorreto encerrar jogo
             self.end_game()
             
 
@@ -82,14 +87,16 @@ class GeniusGame:
     def enable_input(self,enable):
         self.awaiting_input = enable
 
+    #posiciona os botes ao redor de uma circuferencia imaginaria com raio
+    # DRAW_RADIUS e com espacamentos em angulo iguais
     def calculate_scene(self):
         for i, circle in enumerate(self.circles):
             angle = i*2*pi/len(self.circles) + pi/4
             x = int(WIDTH/2 + cos(angle) * DRAW_RADIUS)
             y = int(HEIGHT/2 + sin(angle) * DRAW_RADIUS)
-            #circles.append(Circle((x,y),50,(255,255,255),410))
             circle.move_to((x, y))
 
+    #desenha os elementos da cena na tela
     def draw_scene(self):
         self.window.fill((0, 0, 0))
         for circle in self.circles:
@@ -98,47 +105,53 @@ class GeniusGame:
         text = font.render("Rodada: {}".format(len(self.sequence)), 1, (255, 255, 255))
         self.window.blit(text, (20, 20))
 
-
+    # encerra o jogo
     def end_game(self):
+        #aparece tela vermelha por 1.5 segundos
         self.window.fill((255,0,0))
         action = {"function":self.set_game_over,"arguments":(True,),"delay":1500}
         self.actions_list.append(action)
+        #caso modo seja lucianius aparece imagem e toca som de grito
         if self.difficulty == [True, True]:
             self.window.blit(self.luci,(WIDTH/2-self.luci.get_width()/2, HEIGHT/2-self.luci.get_height()/2))
             self.luci_sound.play()
         else:
+            #caso contrario toca som de buzina
             self.buzzer_sound.play()
+
 
     def set_game_over(self,over):
         self.game_over = over
 
+    #loop de jogo
     def game_loop(self):
+        #cria botoes inicias
         for i in range(4):
             self.circles.append(Circle(50,COLORS[i], FREQUENCIES[i]))
             print(COLORS[i])
         self.calculate_scene()
-            
-        pygame.display.set_caption('LUCIANIUS!')
-        
-        clock = pygame.time.Clock()
-        FPS = 30
-        running_action = False
         self.draw_scene()
+        #adiciona primerio circulo a sequencia correta 
+        self.increment_sequence()
+
+        #variaveis para sistema de delay de funcoes
+        running_action = False
         delay = 0
         timer = 0
-        self.increment_sequence()
+        clock = pygame.time.Clock()
 
         while True:
             # ----- Trata eventos
             dt = clock.tick(FPS)
-
+            # ir para tela game over caso jogo tenha acabado
             if self.game_over == True:
                 return END
+
+            #sistema de delay que nao deixa jogo irresponsivel
             if not running_action and len(self.actions_list) > 0:
                 delay = self.actions_list[0]["delay"]
                 timer = 0
                 running_action = True
-
 
             if running_action:
                 timer += dt
@@ -152,22 +165,21 @@ class GeniusGame:
                         del self.actions_list[0]
                     running_action = False
 
-
+            # tratar dos eventos
             for event in pygame.event.get():
-                # ----- Verifica consequências
+                #fechar jogo caso feche a janela
                 if event.type == pygame.QUIT:
-                    print("quit")
                     return QUIT
+                #ir para menu principal caso aperte esc
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         return INIT
+                #lidar com botao pressionado
                 elif self.awaiting_input and event.type == pygame.MOUSEBUTTONDOWN:
-                        # 1 is the left mouse button, 2 is middle, 3 is right.
                         if event.button == 1:
                             
                             for circle in self.circles:
                                 if circle.colision(event.pos):
                                     self.new_input(circle)                
             
-            # ----- Atualiza estado do jogo
             pygame.display.update()  # Mostra o novo frame para o jogador

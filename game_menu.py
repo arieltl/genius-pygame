511866f0a90c2ec156adaae29152c24a505da518
Lahiry <raphaellcmb@al.insper.edu.br>
2020-06-08 17:02:42 -0300

    WIP

--- game_menu.py
+++ game_menu.py
@@ -1,21 +1,24 @@
+#importa dependencias
 from support_classes import Button
 from CONFIG import WIDTH,HEIGHT,COLORS, QUIT, INIT, GAME
 import pygame
 import os
 
+#inicializa o pygame mixer
 pygame.mixer.pre_init(44100, -16, 2)
 pygame.mixer.init()
 
+#instancia objeto Sound (música do menu)
 music = pygame.mixer.Sound(os.path.join("sprites","korobeiniki.wav"))
 music.set_volume(0.2)
 def main_menu(window, manager):
 
-
+    #cria o título do jogo
     font_path = os.path.join("sprites","PressStart2P-Regular.ttf")
     font = pygame.font.Font(font_path,80)
     text = font.render("LUCIANIUS",True,(255,255,80))
 
-
+    #define as dimensões, coordenadas e cria botões
     b_width = 300
     b_height = 100
     y_pos0 = HEIGHT/2 + 100 -(40+b_height)
@@ -26,30 +29,31 @@ def main_menu(window, manager):
     fast_button = Button(COLORS[1],x_pos1,y_pos0,b_width,b_height,"FAST GENIUS", 50) 
     crazy_button = Button(COLORS[2],x_pos0,y_pos1,b_width,b_height,"CRAZY GENIUS", 50)
     lucianius_button = Button(COLORS[3],x_pos1,y_pos1,b_width,b_height,"LUCIANIUS", 50) 
-    
     buttons = [classic_button,fast_button,crazy_button,lucianius_button]
+
+    #toca música do Tetris infinitamente
     music.play(-1)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return QUIT
             elif event.type == pygame.MOUSEMOTION:
+                #botão reconhece o mouse
                 for button in buttons:
                     button.react_to_mouse(event.pos)
+            #caso botão seja pressionado:
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                #inicia o jogo com a dificuldade de acordo com o botão pressionado
                 if classic_button.isOver(event.pos):
-                    manager.difficulty = [False, False]
-                    return go_to_game()
+                    return go_to_game([False, False], manager)
                 elif fast_button.isOver(event.pos):
-                    manager.difficulty = [True, False]
-                    return go_to_game()
+                    return go_to_game([True, False], manager)
                 elif crazy_button.isOver(event.pos):
-                    manager.difficulty = [False, True]
-                    return go_to_game()
+                    return go_to_game([False, True], manager)
                 elif lucianius_button.isOver(event.pos):
-                    manager.difficulty = [True, True]
-                    return go_to_game()
-                
+                    return go_to_game([True, True], manager)
+
+        #apaga e atualiza o frame   
         window.fill((0,0,0))
         window.blit(text,(WIDTH/2 - text.get_width()/2,90))
 
@@ -57,6 +61,7 @@ def main_menu(window, manager):
             button.draw(window,(180,180,180))
         pygame.display.update()
 
-        def go_to_game():
-            music.stop()
-            return GAME
+def go_to_game(difficulty, manager):
+    manager.difficulty = difficulty
+    music.stop()
+    return GAME
--- game_over_menu.py
+++ game_over_menu.py
@@ -1,47 +1,61 @@
+# importa dependências
 import pygame
 import os 
 from CONFIG import WIDTH,HEIGHT,COLORS,QUIT,GAME,INIT,FPS
 from support_classes import Button
 
+#instancia um objeto Sound que contem a música 
 music = pygame.mixer.Sound(os.path.join("sprites","evil_morty.ogg"))
 music.set_volume(0.4)
 
+
 def game_over(window):
+    #cria texto GAME OVER
     font_path = os.path.join("sprites","PressStart2P-Regular.ttf")
     font = pygame.font.Font(font_path,80)
     text = font.render("GAME OVER",True,COLORS[0])
    
+    #dimensões dos botões
     b_width = 370
     b_height = 120
+    #coordenadas dos botões
     y_pos = HEIGHT/2-b_height + 100
     x_pos0 = WIDTH/2-(40+b_width)
     x_pos1 = WIDTH/2 + 40
     
+    #cria botões
     play_again_b = Button(COLORS[0],x_pos0,y_pos,b_width,b_height,"Play Again",35,font_path)
     main_menu_b = Button(COLORS[0],x_pos1,y_pos,b_width,b_height,"Main Menu",35,font_path)
     buttons = [play_again_b,main_menu_b]
 
+    #toca música infinitamente
     music.play(-1)
     clock = pygame.time.Clock()
     while True:
         clock.tick(FPS)
+        #apaga o frame e desenha um novo 
         window.fill((0,0,0))
         for button in buttons:
             button.draw(window,(180,180,180))
         window.blit(text,(WIDTH/2 - text.get_width()/2,50))
+        #lida com eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return QUIT
             elif event.type == pygame.MOUSEMOTION:
+                #botão reconhece a posição do mouse
                 for button in buttons:
                     button.react_to_mouse(event.pos)
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                #para de tocar a musica 
+                #troca para o menu ou joga de novo
                 if play_again_b.isOver(event.pos):
                     music.stop()
                     return GAME
                 elif main_menu_b.isOver(event.pos):
                     music.stop()
                     return INIT
-            
+
+        #mostra frame  
         pygame.display.update()
     
